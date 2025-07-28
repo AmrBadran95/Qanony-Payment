c00b9802414f0d83975f873d0acf6ac54c9761fa
Amr Badran <badran.amr95@gmail.com>
2025-07-29 02:35:17 +0300

    fix/server routes

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 require("dotenv").config();
 require("./config/firebase");
 
+const app = express();
+
 const paymentRoutes = require("./routes/paymentRoutes");
 const subscriptionControllerRoutes = require("./routes/subscriptionRoutes");
-const webhookRoutes = require("./routes/webhook");
+const subscriptionPaymentRoutes = require("./routes/subscriptionPaymentRoute");
+const webhookRoutes = require("./routes/webhookRoutes");
 const stripeCustomerRoutes = require("./routes/stripeCustomerRoutes");
-const subscriptionPaymentRoute = require("./routes/subscriptionPaymentRoute");
-
-app.use(cors());
 
 app.use("/api/webhook", express.raw({ type: "application/json" }));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/payments", paymentRoutes);
 app.use("/api/subscriptions", subscriptionControllerRoutes);
-app.use("/api/subscriptions", subscriptionPaymentRoute);
+app.use("/api/subscription-payments", subscriptionPaymentRoutes);
+app.use("/api/stripe-customers", stripeCustomerRoutes);
 app.use("/api/webhook", webhookRoutes);
 
 app.get("/", (req, res) => {
