300d79ffe51271823783b50375132a8ae02659c4
Amr Badran <badran.amr95@gmail.com>
2025-07-29 04:24:13 +0300

    fix/server routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const paymentRoutes = require("./routes/paymentRoutes");
 const subscriptionControllerRoutes = require("./routes/subscriptionRoutes");
 const webhookRoutes = require("./routes/webhookRoutes");
 const stripeCustomerRoutes = require("./routes/stripeCustomerRoutes");
+const stripeConnectRoutes = require("./routes/stripeConnectRoutes");
 
 app.use("/api/webhook", express.raw({ type: "application/json" }));
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use("/api/payments", paymentRoutes);
 app.use("/api/subscriptions", subscriptionControllerRoutes);
 app.use("/api/stripe-customers", stripeCustomerRoutes);
+app.use("/api/stripe-connect", stripeConnectRoutes);
 app.use("/api/webhook", webhookRoutes);
 
 app.get("/", (req, res) => {
