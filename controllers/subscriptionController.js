9e73e191e760789f02b2c56076cf1f50070f62df
Amr Badran <badran.amr95@gmail.com>
2025-07-31 01:43:38 +0300

    Added Webhook to server

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -18,6 +18,7 @@ const createLawyerSubscription = async (req, res) => {
       customer: customerId,
       items: [{ price: priceId }],
       payment_behavior: "default_incomplete",
+      collection_method: "charge_automatically",
       payment_settings: {
         payment_method_types: ["card"],
         save_default_payment_method: "on_subscription",
